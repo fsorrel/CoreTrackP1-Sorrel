1f47ee7c84d03e2c438cdfd4cf0e481f78268d7a
fsorrel <fdo.sorrel@gmail.com>
2026-04-07 11:56:13 -0400

    Proyecto CoreTrack primer commit

---
+++ src/main/java/com/example/Fernando_Sorrel/FernandoSorrelApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fernando_Sorrel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FernandoSorrelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FernandoSorrelApplication.class, args);
+	}
+
+}
